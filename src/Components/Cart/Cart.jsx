50f7a07ac50b32cbe63ff7f0046bae3f82c9b9b5
Mahadi001 <sadat.himel@gmail.com>
2021-06-14 17:35:43 +0600

    add total

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
 const Cart = ({cart}) => {
+    const courseTotal = cart.reduce((sum,course) => sum + course.price, 0);
+    const tax = courseTotal/10;
 
-    const courseTotal = cart.reduce((sum,course)=> sum + course.price,0);
     return (
         <div>
             <h1>Total Item order: {cart.length}</h1>
             <p>Price: {courseTotal}</p>
+            <p>Tax: {tax}</p>
+            <p>Grand Total: {courseTotal + tax}</p>
+            <button>Place Oder{}</button>
         </div>
     );
 };
--- src/fakeData/courseData.js
+++ src/fakeData/courseData.js
@@ -4,21 +4,21 @@ var courseData = [
         name: "javaScript",
         instructor: "SadatHimel",
         image: " https://i.ibb.co/qnQprq4/java-script.jpg",
-        price: "1000"
+        price: 1000
     },
     {
         id: 2,
         name: "Paython",
         instructor: "Tanvir ahmed",
         image: "https://i.ibb.co/njJj98h/reactjs.jpg",
-        price: "2000"
+        price: 2000
     },
     {
         id: 3,
         name: "ReactJs",
         instructor: "HimelSadat",
         image: "https://i.ibb.co/hsJkN5Z/paython.jpg",
-        price: "1500"
+        price: 1500
     }
 ];
 
